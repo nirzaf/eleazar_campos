edb9fc7a32917d459da04402e623e773713afcfa
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2025-12-07 10:39:54 +0000

    feat: add favicon to the application
    Include favicon from profile.png and dynamically add link element if not present

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import faviconUrl from '../profile.png'
 
 const rootEl = document.getElementById('root')
+const existingIcon = document.querySelector('link[rel="icon"]')
+const linkIcon = existingIcon || document.createElement('link')
+linkIcon.rel = 'icon'
+linkIcon.type = 'image/png'
+linkIcon.href = faviconUrl
+if (!existingIcon) document.head.appendChild(linkIcon)
 createRoot(rootEl).render(
   <React.StrictMode>
     <App />
